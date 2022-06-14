bc2cbd63a43f1f862b9351ea383964c321f81000
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-14 21:05:15 +0100

    Add style to the quotes page

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,7 +1,29 @@
 const Quote = () => (
 
-  <div className="contenu">
-    While it may seem like math problems like the above have no real use in life,
+  <div className="quotes">
+    <ul>
+      <li>
+        Without mathematics, there’s nothing you can do. Everything around you is mathematics.
+        Everything around you is numbers.
+        {' '}
+        <span> — Shakuntala Devi, Indian writer and mental calculator</span>
+      </li>
+      <br />
+      <li>
+        Mathematics as an expression of the human mind reflects the active will,
+        the contemplative reason,
+        and the desire for aesthetic perfection. Its basic elements are logic and intuition,
+        analysis and construction, generality and individuality.
+        {' '}
+        <span>— Richard Courant, German-American mathematician</span>
+      </li>
+      <br />
+      <li>
+        What is mathematics? It is only a systematic effort of solving puzzles posed by nature.
+        {' '}
+        <span>— Shakuntala Devi</span>
+      </li>
+    </ul>
   </div>
 );
 
