47d4f45aed45d13a6be29cdc8888142dc8f4b329
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-16 10:18:01 +0100

    add test for the operate and calculator

---
+++ src/tests/calculator.test.js
@@ -0,0 +1,35 @@
+import calculate from '../logic/calculate';
+
+describe('Test for  calculate function', () => {
+  test('Test AC button', () => {
+    const obj = {
+      next: null,
+      total: 5,
+      operation: '+',
+    };
+
+    const result = calculate(obj, 'AC');
+
+    expect(result).toStrictEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('Test Equal button', () => {
+    const obj = {
+      next: 3,
+      total: 5,
+      operation: '+',
+    };
+
+    const result = calculate(obj, '=');
+
+    expect(result).toStrictEqual({
+      total: "8",
+      next: null,
+      operation: null,
+    });
+  });
+});
